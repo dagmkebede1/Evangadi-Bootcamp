4ce22f795e52ec2d0f2d0f92e28b41db2a7465f8
samsonmeseret <samsonmeseretb@gmail.com>
2022-11-03 14:53:37 -0400

    random number generating function

---
+++ Phase_Three/nodeModulesProject/randonNumber.js
@@ -0,0 +1,16 @@
+// The "randomNumber" module has a function called random(). The random
+// function just returns a random number when it gets executed
+// b. Execute the function inside the module
+// c. Save the returned value in a variable and log the variable on the console. Now, run
+// your module on the terminal to see the printed output
+// d. Export your module so that it is accessible to other module
+
+function random() {
+  return Math.round(Math.random() * 10); // 0 - 1: max: 0.999, min: 0.001
+  //5.5 --- 6
+  //5.4 --- 5
+}
+
+let result = random();
+
+console.log(result);
